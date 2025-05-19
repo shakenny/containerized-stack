4d0e956adde80930da15c8c23f15990f243d4bd8
Kenny Yang <ybwpcl@hotmail.com>
2025-05-19 21:00:31 +0800

    chore: 更新本地初始化脚本以下载并设置权限
    更新 `main_local.sh` 脚本，使其在初始化环境后下载 `.init_env.sh` 文件并设置可执行权限。这确保了脚本在本地环境中可以正确执行。

--- main_local.sh
+++ main_local.sh
@@ -1,1 +1,6 @@
 bash <(curl -sSL https://raw.githubusercontent.com/shakenny/containerized-stack/refs/heads/main/local/init_env.sh)
+
+
+cd ~ || exit
+wget -O ~/.init_env.sh https://raw.githubusercontent.com/shakenny/containerized-stack/refs/heads/main/local/init_env.sh
+chmod +x ~/.init_env.sh
